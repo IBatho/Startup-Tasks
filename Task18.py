8bd2ec9570ceba6d36792b2b3d52f58cdb7efdd3
Isaac Batho <isaacbatho@gmail.com>
2026-01-17 20:24:59 +0000

    visualisation and refined learning

--- Task18.py
+++ Task18.py
@@ -2,6 +2,30 @@ import gymnasium as gym
 from stable_baselines3 import DQN
 from number_guesser_env import number_guesser_env
 
-env = number_guesser_env()
-model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=20000)
+env = number_guesser_env(render_mode="human")
+model = DQN("MlpPolicy", 
+            env, 
+            verbose=1, 
+            exploration_fraction=0.2,
+            exploration_final_eps=0.05,)
+model.learn(total_timesteps=100000)
+obs, info = env.reset()
+
+for _ in range(100):
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, terminated, truncated, info = env.step(action)
+    env.render()
+    print(obs, reward, terminated, truncated, info)
+    if terminated or truncated:
+        obs, info = env.reset()
+        print(terminated, truncated)
+
+
+'''for _ in range(10):
+    action = env.action_space.sample()
+    obs, reward, done, truncated, info = env.step(action)
+    env.render()
+
+    if done or truncated:
+        print("episode ended")
+        break'''
--- number_guesser_env.py
+++ number_guesser_env.py
@@ -2,11 +2,12 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 import math
 
 class number_guesser_env(gym.Env):
     render_mode = ["human"]
-    def __init__(self, low=1, high=100, max_steps=100, render_mode=None):
+    def __init__(self, low=1, high=100, max_steps=50, render_mode=None):
         super().__init__()
         self.low = low
         self.high = high
@@ -21,6 +22,7 @@ class number_guesser_env(gym.Env):
         self.max_steps = max_steps
         self.step_count = 0
         self.last_guess = None
+        self.fig, self.ax = None, None
     
 
 
@@ -29,7 +31,7 @@ class number_guesser_env(gym.Env):
         super().reset(seed=seed)
         self.target = self.np_random.integers(self.low, self.high + 1)
         self.last_guess = self.low
-        difference = self.last_guess - self.target
+        difference = abs(self.last_guess - self.target)
         self.step_count = 0
         obs = np.array([self.last_guess, difference], dtype=np.int32)
         return obs, {}
@@ -38,12 +40,11 @@ class number_guesser_env(gym.Env):
     def step(self, action):
         done = False
         truncated = False
-        if self.step_count == self.max_steps:
+        if self.step_count >= self.max_steps:
             truncated = True
-            guess = int(action) + self.low
-            self.last_guess = guess
-            difference = guess - self.target
+            difference = self.last_guess - self.target
             obs = np.array([self.last_guess, difference], dtype=np.int32)
+            print("truncated")
             return obs, 0.0, done, truncated, {}
         
         guess = int(action) + self.low
@@ -51,17 +52,40 @@ class number_guesser_env(gym.Env):
         difference = guess - self.target
 
         if guess == self.target:
-            reward = 1
+            reward = 100
             done = True
+            print("done")
         else:
-            alpha = 0.1
-            reward = float(math.exp(-abs(alpha*difference)))
-        
+            alpha = 0.3
+
+            reward = -10*abs(difference/99) -0.1 #float(-(math.exp(abs(difference))))
         obs = np.array([self.last_guess, difference], dtype=np.int32)
         self.step_count += 1
+        #print(obs, reward, done, truncated, {})
         return obs, reward, done, truncated, {}
     
     def render(self):
         if self.render_mode != "human":
             return
         print(f"Target: {self.target}, Last Guess: {self.last_guess}")
+
+        if self.fig is None:
+            self.fig, self.ax = plt.subplots()
+            plt.ion()
+        self.ax.clear()
+        self.ax.set_xlim(self.low-1, self.high+1)
+        self.ax.set_ylim(-1,1)
+        self.ax.hlines(0, self.low, self.high, color="black")
+        self.ax.set_xticks(range(self.low, self.high + 1))
+        self.ax.text(self.target, 0.1, "X", ha = "center", va="bottom", color="black", fontsize=14)
+
+        if self.last_guess is not None:
+            self.ax.scatter([self.last_guess], [0], c="red", s=50)
+
+            self.ax.annotate(
+                "",
+                xy=(self.target,0),
+                xytext=(self.last_guess, 0),
+                arrowprops=dict(arrowstyle="->", color="blue"),
+                )
+        plt.pause(0.001)
