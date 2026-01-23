460149f7d4b30b9ede3f1421634a0d96d71f53c4
Isaac Batho <isaacbatho@gmail.com>
2026-01-23 21:08:56 +0000

    Initial training - not great

--- Task19.py
+++ Task19.py
@@ -1,7 +1,40 @@
 import gymnasium as gym
+from stable_baselines3 import DQN, PPO
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 from simple_factory_env import SimpleFactoryEnv
 
 env = SimpleFactoryEnv()
+
+'''model = DQN("MlpPolicy", 
+            env, 
+            verbose=1, 
+            exploration_fraction=0.3,
+            exploration_final_eps=0.05)'''
+
+model = PPO(
+    "MlpPolicy",
+    env,
+    verbose=1,
+    learning_rate=0.0003,
+    n_steps=2048,
+    batch_size=64,
+    n_epochs=10,
+    gamma=0.95,
+    ent_coef=0.01,
+    policy_kwargs=dict(
+        net_arch=[256, 256]  # Larger network
+    )
+)
+model.learn(total_timesteps=20000)
+obs, info = env.reset()
+
+for _ in range(100):
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, terminated, truncated, info = env.step(action)
+    #env.render()
+    print(obs, reward, terminated, truncated, info)
+    if terminated or truncated:
+        obs, info = env.reset()
+        print(terminated, truncated)
--- simple_factory_env.py
+++ simple_factory_env.py
@@ -11,25 +11,26 @@ class SimpleFactoryEnv(gym.Env):
         self.order = None
         self.busy = np.array([0,0], dtype=np.int32)
         self.time_remaining = np.array([self.A_time, self.B_time], dtype=np.int32)
-        self.action_space = gym.spaces.Discrete(3)              # 3 potential actions, 1 for move or 0 for hold
-        self.observation_space = gym.Spaces.Box(
+        self.action_space = gym.spaces.Discrete(2)              # 3 potential actions, 1 for move or 0 for hold
+        self.observation_space = gym.spaces.Box(
             low=np.array([0,0,0.0,0.0]), 
-            high=np.array([1,1,self.A_time, self.B_time]), shape=(4,), dtype=np.float32
-        )
-        self.current_state = np.array([self.busy, self.busy, self.A_time, self.B_time]) # flat vector makes proccessing easier
+            high=np.array([1, 1, self.A_time, self.B_time]), shape=(4,), dtype=np.float32
+        ) # flat vector makes proccessing easier
+        self.current_state = np.array([self.busy, self.time_remaining]) 
         self.to_do = None
         self.max_steps = max_steps
         self.step_count = 0
         self.to_do = self.order
         self.doing = None 
         self.complete = None
+        self.next_state = self.current_state
 
 
         
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
-        self.order = np.random.random_integers(0, 100)
+        self.order = np.random.randint(0, 101)
         self.A_time = 4
         self.B_time = 3
         self.time_remaining = np.array([self.A_time, self.B_time], dtype=np.int32)
@@ -38,7 +39,7 @@ class SimpleFactoryEnv(gym.Env):
         self.current_state = np.array([self.busy, self.time_remaining])
         obs = self.current_state.flatten().astype(float)
         self.step_count = 0
-        self.next_state = None
+        self.next_state = self.current_state
         self.to_do = self.order # number of orders
         self.doing = 0
         self.complete = 0
@@ -46,8 +47,9 @@ class SimpleFactoryEnv(gym.Env):
 
 
     def step(self, action):
-        done = False
+        terminated = False
         truncated = False 
+        reward = 0
         a_current_busy = self.busy[0]
         b_current_busy = self.busy[1]
                              # if ran out of steps then end episode
@@ -55,17 +57,21 @@ class SimpleFactoryEnv(gym.Env):
         if self.step_count >= self.max_steps:
             truncated = True
             obs = self.current_state.flatten().astype(float)
-            return obs, reward, done, truncated, {}
+            reward = -1
+            return obs, reward, terminated, truncated, {}
         # if there are no more orders to complete then done and reward
         elif self.to_do == 0 and self.doing == 0:     
-            reward = 100
-            done = True
+            reward = 200
+            terminated = True
             obs = self.current_state.flatten().astype(float)
-            return obs, reward, done, truncated, {}
+            return obs, reward, terminated, truncated, {}
         
         # If action is to stay then decrease time remaining
         if action == 0:
-            self.time_remaining = -1
+            if self.busy[0] == 1:
+                self.time_remaining[0] -= 1
+            if self.busy[1] == 1:
+                self.time_remaining[1] -= 1
             self.next_state = np.array([self.busy, self.time_remaining])
 
         # If action is to move: reset FU times remaining at end
@@ -73,7 +79,7 @@ class SimpleFactoryEnv(gym.Env):
 
             
             # If A (FU before another FU) is not currently busy (can be made into loop for more FUs) 
-            # and an order is added the next state will only have A as busy and B will be free
+            # and an order is added the next state will only have A as busy and B will be free e.g. at the start of order
             if self.busy[0] == 0:
                 self.busy[0] += 1
 
@@ -83,14 +89,15 @@ class SimpleFactoryEnv(gym.Env):
                 self.busy[1] += 1
 
             else: 
-                #increase busyness by 1 
+                #increase busyness by 1 becasue order is added
                 self.busy += 1
 
-            # If the time remaining to completion of the current state is less than or equal to 0 then FU is done and can pass on
-            # to next FU so busyness becomes 0 because complete and asked to move on. less then 0 because item may be held in FU after complete.
-            # made 0 instead of taking away 1 because anything greater than 1 would have stopped episode
+            # If the time remaining to completion of the current state is less than or equal to 0 then FU is done with item and can pass on
+            # to next FU or complete zone so busyness decreases. less then 0 because item may be held in FU after complete.
+            # Taking away 1 because added before for new item but if this item can move it reduces the business
             if self.time_remaining[0] <= 0:
                 self.busy[0] -= 1
+
             if self.time_remaining[1] <= 0:
                 self.busy[1] -= 1
                 self.doing -= 1
@@ -99,6 +106,8 @@ class SimpleFactoryEnv(gym.Env):
             if self.to_do > 0:
                 self.doing +=1
                 self.to_do -= 1
+            else:
+                print('complete all orders')
 
             self.time_remaining = np.array([self.A_time, self.B_time], dtype=np.int32)
         
@@ -107,22 +116,26 @@ class SimpleFactoryEnv(gym.Env):
 
 
         # reasons for an invalid move:
-        # if the either FUs in current state still has to complete it's action and next state has moved on then negative reward
-        if self.current_state[[1,0]] > 0 and self.next_state[[1,0]] == self.A_time \
-            or self.current_state[[1,1]] > 0 and self.next_state[[1,1]] == self.B_time \
-            :
-            reward = -1
+        # if the either FUs have more FU than allowed (1) then negative reward and terminate. This means due to previous code that item 
+        # didn't complete it's time in the FU.
+        if self.busy[0] > 1 or self.busy[1] > 1:
+            reward = -5
+            terminated = True
 
         # Reasons for positive rewards:
         # FU finishes a single unit
-        elif self.next_state[[1,0]] == 0 or self.next_state[[1,1]] == 0:
-            reward = 1
-        
+        elif self.current_state[0][0] == 0 or self.current_state[0][1] == 0 and self.busy[0] == 1:
+            reward = 40
+        elif self.time_remaining[0] < 0 or self.time_remaining[1] < 0:
+            reward = self.time_remaining 
 
         self.current_state = self.next_state
+    
         obs = self.current_state.flatten().astype(float)
 
         self.step_count += 1
-        return obs, reward, done, truncated, {}
+        print(f"Current State: {self.current_state}, Action: {action}, Steps: {self.step_count}, To Do: {self.to_do}, \
+              Doing: {self.doing}, Complete: {self.complete}, Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
+        return obs, reward, terminated, truncated, {}
 
 
