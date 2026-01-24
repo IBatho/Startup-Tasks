28e82c904799f1d7738402344b076bf5d5ead174
Isaac Batho <isaacbatho@gmail.com>
2026-01-24 21:14:23 +0000

    Updating logic because wasn't learning

--- simple_factory_env.py
+++ simple_factory_env.py
@@ -9,8 +9,6 @@ class SimpleFactoryEnv(gym.Env):
         self.A_time = 4
         self.B_time = 3
         self.order = None
-        self.busy = np.array([0,0], dtype=np.int32)
-        self.time_remaining = np.array([self.A_time, self.B_time], dtype=np.int32)
         self.action_space = gym.spaces.Discrete(2)              # 3 potential actions, 1 for move or 0 for hold
         self.observation_space = gym.spaces.Box(
             low=np.array([0,0,0.0,0.0]), 
@@ -26,93 +24,123 @@ class SimpleFactoryEnv(gym.Env):
         self.next_state = self.current_state
 
 
+
         
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
-        self.order = np.random.randint(0, 101)
-        self.A_time = 4
-        self.B_time = 3
-        self.time_remaining = np.array([self.A_time, self.B_time], dtype=np.int32)
-
-        self.busy = np.array([0,0], dtype=np.int32)
-        self.current_state = np.array([self.busy, self.time_remaining])
-        obs = self.current_state.flatten().astype(float)
+        self.total_orders = np.random.randint(5, 16)
+        self.to_do = self.total_orders # number of orders
+        self.A_duration = 4 # duration to complete working at FU A
+        self.B_duration = 3
+        self.A_time = 0 # time A has been working
+        self.B_time = 0
+        self.A_busy = 0
+        self.B_busy = 0
+        self.busy = self._get_busy()
+        self.time_working = self._get_time()
+        self.current_state = self._get_state()
         self.step_count = 0
         self.next_state = self.current_state
-        self.to_do = self.order # number of orders
-        self.doing = 0
+        self.doing = self.A_busy + self.B_busy
         self.complete = 0
-        return obs, {}
-
+        return self._get_obs(), {}
+    
+    def _get_obs(self):
+        obs = self.current_state.flatten().astype(float)
+        return obs
+    
+    def _get_busy(self):
+        self.busy = np.array([self.A_busy, self.B_busy], dtype=np.int32)
+        return self.busy
+    
+    def _get_time(self):
+        self.time_working = np.array([self.A_time, self.B_time], dtype=np.float32)
+        return self.time_working
+    
+    def _get_state(self):
+        self.current_state = np.array([self._get_busy(), self._get_time])
+        return self.current_state
 
     def step(self, action):
+        self.step_count += 1
         terminated = False
         truncated = False 
         reward = 0
-        a_current_busy = self.busy[0]
-        b_current_busy = self.busy[1]
-                             # if ran out of steps then end episode
-
-        if self.step_count >= self.max_steps:
-            truncated = True
-            obs = self.current_state.flatten().astype(float)
-            reward = -1
-            return obs, reward, terminated, truncated, {}
         # if there are no more orders to complete then done and reward
-        elif self.to_do == 0 and self.doing == 0:     
-            reward = 200
+        if self.to_do == 0 and self.doing == 0:     
+            reward += 200
             terminated = True
-            obs = self.current_state.flatten().astype(float)
-            return obs, reward, terminated, truncated, {}
+            return self._get_obs, reward, terminated, truncated, {}
+        # if ran out of steps then end episode
+        elif self.step_count >= self.max_steps:
+            truncated = True
+            reward -= 1
+            return self._get_obs, reward, terminated, truncated, {}
+
         
         # If action is to stay then decrease time remaining
+        # If the time working is longer than neccessary decrease the reward
         if action == 0:
-            if self.busy[0] == 1:
-                self.time_remaining[0] -= 1
-            if self.busy[1] == 1:
-                self.time_remaining[1] -= 1
-            self.next_state = np.array([self.busy, self.time_remaining])
+            if self.A_busy == 1:
+                self.A_time += 1
+                if self.A_time > self.A_duration:
+                    reward -= 0.5
+
+            if self.B_busy == 1:
+                self.B_time += 1
+                if self.B_time > self.B_duration:
+                    reward -= 0.5
+
+            self.next_state = self._get_state()
+
 
         # If action is to move: reset FU times remaining at end
         elif action == 1:
-
-            
             # If A (FU before another FU) is not currently busy (can be made into loop for more FUs) 
             # and an order is added the next state will only have A as busy and B will be free e.g. at the start of order
-            if self.busy[0] == 0:
-                self.busy[0] += 1
+            if self.A_busy == 0:
+                self.A_busy += 1
 
             # If no more orders to complete and units move to next FU so A won't be busy and B will be
             elif self.to_do == 0:
-                self.busy[0] = 0
-                self.busy[1] += 1
+                self.B_busy += 1
 
             else: 
                 #increase busyness by 1 becasue order is added
-                self.busy += 1
+                self.A_busy += 1
+                self.B_busy += 1
 
-            # If the time remaining to completion of the current state is less than or equal to 0 then FU is done with item and can pass on
-            # to next FU or complete zone so busyness decreases. less then 0 because item may be held in FU after complete.
-            # Taking away 1 because added before for new item but if this item can move it reduces the business
-            if self.time_remaining[0] <= 0:
-                self.busy[0] -= 1
-
-            if self.time_remaining[1] <= 0:
-                self.busy[1] -= 1
-                self.doing -= 1
+   
+            # Taking away 1 because added before for new item but if this item can move it reduces the busyness
+            if self.A_time >= self.A_duration:
+                self.A_busy -= 1
+                self.B_busy += 1
                 self.complete += 1
+                reward += 1
+            else:
+                reward -= 10
+                terminated = True
+                # edited logic up to here
+
+            if self.B_time >= self.B_duration:
+                self.B_busy -= 1
+                self.complete += 1
+                reward += 10
+            else:
+                reward -= 10
+                terminated = True
+            
 
             if self.to_do > 0:
-                self.doing +=1
                 self.to_do -= 1
-            else:
+            elif self.complete == self.total_orders:
                 print('complete all orders')
 
-            self.time_remaining = np.array([self.A_time, self.B_time], dtype=np.int32)
         
-            self.next_state = np.array([self.busy, self.time_remaining])
-        
+            self.next_state = self._get_state()
+            # edited logic up to here
+
 
 
         # reasons for an invalid move:
@@ -133,7 +161,7 @@ class SimpleFactoryEnv(gym.Env):
     
         obs = self.current_state.flatten().astype(float)
 
-        self.step_count += 1
+
         print(f"Current State: {self.current_state}, Action: {action}, Steps: {self.step_count}, To Do: {self.to_do}, \
               Doing: {self.doing}, Complete: {self.complete}, Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
         return obs, reward, terminated, truncated, {}
