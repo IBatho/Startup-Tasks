fa8d12bb2c93df51d4b0f888a22e3c5e94a63a4e
Isaac Batho <isaacbatho@gmail.com>
2026-02-20 18:50:57 +0000

    plotting

--- plotter.py
+++ plotter.py
@@ -9,11 +9,18 @@ y = data["Value"]
 coefs = np.polyfit(x, y, deg=3)
 trendline = np.poly1d(coefs)
 plt.figure(figsize=(10, 6))
-plt.scatter(x,y,color="red", alpha = 0.8, label="Raw Data", s=10)
-plt.plot(x, trendline(x), color="steelblue", linewidth=2, label="Trendline")
-plt.legend()
-plt.xlabel("Timesteps")
-plt.ylabel("Mean Episode Reward")
-plt.title("PPO: Graph of Mean Episode Reward vs Timesteps")
+plt.scatter(x,y,color="red", alpha = 0.8, label="Raw Data", s=30)
+plt.plot(x, trendline(x), color="steelblue", linewidth=4, label="Trendline")
+plt.legend(
+    fontsize=25,          # Increases text size
+    title_fontsize=25,    # Increases title size
+    loc='lower right',     # Moves it so it doesn't overlap data
+    labelspacing=1.2      # Adds vertical space between entries
+)
+plt.xlabel("Timesteps", fontsize=30, fontweight="bold")
+plt.ylabel("Mean Episode Reward", fontsize=30, fontweight="bold")
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.title("PPO: Graph of Mean Episode Reward vs Timesteps", fontsize=30, fontweight="bold")
 plt.grid(True, linestyle="--", alpha=0.7)
 plt.show()
